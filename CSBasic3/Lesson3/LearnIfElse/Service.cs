c2a393b3fc91e1dcb82453af8e9ade33f4717695
Vu Le <lhlvhsg@gmail.com>
2023-12-27 21:56:02 +0700

    learn if else

---
+++ CSBasic3/Lesson3/LearnIfElse/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnIfElse
+{
+    public class Service
+    {
+        // xác định học lực: điểm <5 => bad,, =5 => average > 5 => good
+        public string EvaluateStudent(double score)
+        {
+            string result = "";
+            if (score == 5)
+            {
+                result = "average";
+            }
+
+            if (score < 5)
+
+            {
+                result = "bad";
+            }
+
+            if (score > 5)
+
+            {
+                result = "good";
+            }
+            return result;
+        }
+
+    }
+}
