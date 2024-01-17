3722e50960d8a66c672f97a84955b2dbea74da81
Vu Le <lhlvhsg@gmail.com>
2024-01-17 19:21:24 +0700

    learn do while

--- CSBasic3/LearnLoop/Service.cs
+++ CSBasic3/LearnLoop/Service.cs
@@ -123,5 +123,16 @@ namespace LearnLoop
                 check = double.TryParse(Console.ReadLine(), out score) && score >= 0 && score <= 10;
             }
         }
+
+        public static void LearnDoWhile()
+        {
+            int i = 5;
+            do
+            {
+                Console.WriteLine(i);
+
+            }
+            while (i < 5);
+        }
     }
 }
