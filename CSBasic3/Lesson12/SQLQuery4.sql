c496db08852931e44b2b1d140997312e60aefc69
Vu Le <lhlvhsg@gmail.com>
2024-02-26 21:59:59 +0700

    SQL-lesson12

---
+++ CSBasic3/Lesson12/SQLQuery4.sql
@@ -0,0 +1,11 @@
+﻿insert [dbo].[Categories] ([CategoryId], [CategoryName], [Status], [CreatedDate])
+values
+(NEWID(), N'Điện thoại', 1, GETDATE())
+
+insert [dbo].[Users] ([UserId], [UserName], [Password], [Email], [Mobile], [Status], [CreatedDate])
+values
+(NEWID(), N'Trần Văn Tuấn', N'123456', N'tuantv@gmail.com', N'0961399999', 1, GETDATE())
+
+go
+select * from [dbo].[Categories];
+select * from Users;
